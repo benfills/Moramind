8d219e047a56bd38f62b37bdd42e843c5f05ff09
benfills <bigsmokeyeechungus@gmail.com>
2026-04-16 10:41:56 +0700

    Timer: Fixed  Timer Stale Closure

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,95 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+// issue 1 = Dependency Issue: Stale tick state due to missing useEffect dependency
+// issue 2 = Closure bug: tick is using an outdated reference of the function
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import { ImageBackground } from "expo-image";
+import { useState, useEffect, useRef } from "react";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+export default function StudyTime() {
+    const [tick, setTick] = useState(1);
+    const [status, setStatus] = useState(0);
+    const tickref = useRef(0);
+    console.log(tickref.current);
+    useEffect(() => {
+        if (status === 1) {
+          if (tickref.current !== 0) {
+            const intervalID = setInterval(() => {
+            if (tickref.current !== 0) setTick((prev) => prev - 1);
+            else clearInterval(intervalID);
+        }, 100);
+        console.log(intervalID);
+        return () => clearInterval(intervalID);
+        }
+    }
+    }, [status]);
+    useEffect(() => {
+        tickref.current = tick;
+    }, [tick]);
+    if (tick !== 0) {
+      return (
+      <ImageBackground source={require('/home/benfills/MobileApp/MoramindProject/Moramind/assets/images/MainWallpaper.webp')} style={styles.container} contentFit={'fill'}>
+      <View style={styles.container}>
+          <Text> Study Time {tick} </Text>
+          <Text> current Status: {status} </Text>
+          <Text> current ref state {tickref.current} </Text>
+          <Pressable
+          style={styles.button}
+          onPress={() => {
+            if (status === 0 && tick > 0) {
+              setStatus((prev) => prev + 1);
+            } else if (status === 0 && tick <= 0) {
+              alert("Please Set Your Timer First");
+            } else setStatus((prev) => prev - 1);
+          }}
+        >
+          <Text> Update Status </Text>
+        </Pressable>
+        <Pressable
+          style={styles.button}
+          onPress={() => setTick((prev) => prev + 60)}
+        >
+          <Text> Increase to 60</Text>
+        </Pressable>
+        <Pressable
+          style={styles.button}
+          onPress={() => setTick((prev) => prev + 120)}
+        >
+          <Text> Increase to 120 </Text>
+        </Pressable>
+      </View>
+      </ImageBackground>
+    );
+  }
+}
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+function RestTime() {
+  const [tick1, setTick1] = useState(0)
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#f4f4f4",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: 10,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  button: {
+    backgroundColor: "#ffbb00",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 4,
+    },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    textAlign: "center",
   },
+
 });
