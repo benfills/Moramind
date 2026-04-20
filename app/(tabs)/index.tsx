a1d413b0fb2d5ceb69808bd820daf85e4e1e6156
benfills <bigsmokeyeechungus@gmail.com>
2026-04-20 07:52:50 +0700

    Enhance StudyTime component: fix stale closure issue, improve tick handling, and add rest time functionality. Update wallpapers and button labels for clarity.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+// stale closure: tickref closure pada saat status = 1 dan harus mutasi status dari 0 ke 1 lagi
 import { ImageBackground } from "expo-image";
 import { useState, useEffect, useRef } from "react";
 import { View, Text, StyleSheet, Pressable } from "react-native";
@@ -9,6 +10,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#f4f4f4",
     alignItems: "center",
     justifyContent: "center",
+  },
+  container2: {
+    flex: 1
   },
   title: {
     fontSize: 24,
@@ -32,27 +36,29 @@ const styles = StyleSheet.create({
 export default function StudyTime() {
     const [tick, setTick] = useState(1);
     const [status, setStatus] = useState(0);
+    const [rest, setRest] = useState(0)
     const tickref = useRef(0);
-    console.log(tickref.current);
+    const restref = useRef(0)
     useEffect(() => {
         if (status === 1) {
           if (tickref.current !== 0) {
             const intervalID = setInterval(() => {
-            if (tickref.current !== 0) setTick((prev) => prev - 1);
-            else clearInterval(intervalID);
+            if (tickref.current !== 0) setTick(prev => prev - 1);
+            else if (tickref.current === 0) clearInterval(intervalID);
+            else
         }, 100);
-        console.log(intervalID);
         return () => clearInterval(intervalID);
         }
     }
     }, [status]);
     useEffect(() => {
-        tickref.current = tick;
+      tickref.current = tick;
     }, [tick]);
-
-    if (tick <= 0) {
+    console.log("tickref: ",tickref.current)
+    console.log("status: ", status)
+    if (tick > 0) {
       return (
-      <ImageBackground source={require('/home/benfills/MobileApp/MoramindProject/Moramind/assets/images/MainWallpaper.webp')} style={styles.container} contentFit={'fill'}>
+      <ImageBackground source={require('/home/benfills/MobileApp/MoramindProject/Moramind/assets/images/StudyWallpaper.webp')} style={styles.container} contentFit={'fill'}>
       <View style={styles.container}>
           <Text> Study Time {tick} </Text>
           <Pressable
@@ -71,24 +77,42 @@ export default function StudyTime() {
           style={styles.button}
           onPress={() => setTick((prev) => prev + 60)}
         >
-          <Text> Increase to 60</Text>
+          <Text> Increase Study Time to 60</Text>
         </Pressable>
         <Pressable
           style={styles.button}
           onPress={() => setTick((prev) => prev + 120)}
         >
-          <Text> Increase to 120 </Text>
+          <Text> Increase Study Time to 120 </Text>
+        </Pressable>
+        <Pressable>
+          <Text> Increase Rest Time to 60 </Text>
+        </Pressable>
+        <Pressable>
+          <Text> Increase Rest Time to 120 </Text>
         </Pressable>
       </View>
       </ImageBackground>
     )}
     else {
       return (
-      <ImageBackground source={require('/home/benfills/MobileApp/MoramindProject/Moramind/assets/images/MainWallpaper.webp')} style={styles.container} contentFit={'fill'}>
-      <View>
-        <Text> Rest Time </Text>
+      <ImageBackground source={require('/home/benfills/MobileApp/MoramindProject/Moramind/assets/images/BreakWallpaper.webp')} style={styles.container} contentFit={'fill'}>
+      <View style={styles.container2}>
+          <Text> Rest Time {rest} </Text>
       </View>
       </ImageBackground>
       )
     }
+}
+
+function StartStudy() {
+  return (
+    <View>
+      <Text> Start </Text>
+    </View>
+  )
+}
+
+function StartBreak() {
+  
 }
